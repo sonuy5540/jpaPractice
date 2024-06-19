870766750b02055532baed35977e09bcc923124b
c0844501 <c0844501@mylambton.ca>
2024-06-19 00:33:54 -0400

    first commit

---
+++ src/test/java/com/example/springdemojpa/SpringDemoJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springdemojpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDemoJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
